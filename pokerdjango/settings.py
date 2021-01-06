42002627d36166c0e39eb5db81559ca2a59fe3e1
Nicolas Sursock (work) <nwsursock@Nicolass-MacBook-Pro.local>
2021-01-06 17:32:43 +0200

    Fixed database issues

--- pokerdjango/settings.py
+++ pokerdjango/settings.py
@@ -86,15 +86,15 @@ WSGI_APPLICATION = 'pokerdjango.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    #'default': {
-    #    'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': BASE_DIR / 'db.sqlite3',
-    #}
-    'default': dj_database_url.config(
-       # Feel free to alter this value to suit your needs.
-       default='postgresql://postgres:postgres@localhost:5432/mysite',
-       conn_max_age=600
-   )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+    #'default': dj_database_url.config(
+    #   # Feel free to alter this value to suit your needs.
+    #   default='postgresql://postgres:postgres@localhost:5432/mysite',
+    #   conn_max_age=600
+    #)
 }
 
 
