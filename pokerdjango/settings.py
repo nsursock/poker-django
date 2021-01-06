431d713a10f953e54a580b8b2418c6ce3be8500d
Nicolas Sursock (work) <nwsursock@Nicolass-MacBook-Pro.local>
2021-01-06 17:08:42 +0200

    Added build scripts

--- pokerdjango/settings.py
+++ pokerdjango/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,16 +22,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-import os
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'xw0@ytc$42hv5lqp=gj+movwvb1l1n$&ro3i3comobtqljr0hq')
-#SECRET_KEY = 'xw0@ytc$42hv5lqp=gj+movwvb1l1n$&ro3i3comobtqljr0hq'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+#DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = 'RENDER' not in os.environ
 #DEBUG = True
 
 ALLOWED_HOSTS = []
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+   ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
@@ -57,8 +61,6 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 ROOT_URLCONF = 'pokerdjango.urls'
 
 TEMPLATES = [
@@ -84,10 +86,15 @@ WSGI_APPLICATION = 'pokerdjango.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+    'default': dj_database_url.config(
+       # Feel free to alter this value to suit your needs.
+       default='postgresql://postgres:postgres@localhost:5432/mysite',
+       conn_max_age=600
+   )
 }
 
 
@@ -128,3 +135,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Following settings only make sense on production and may break development environments.
+if not DEBUG:
+   # Tell Django to copy statics to the `staticfiles` directory
+   # in your application directory on Render.
+   STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+   # Turn on WhiteNoise storage backend that takes care of compressing static files
+   # and creating unique names for each version so they can safely be cached forever.
+   STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
